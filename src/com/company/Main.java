7cdfbe9867a4ac835c501874e291f7e6e3818f02
Federico Carbonell <fed.carbonell@gmail.com>
2019-03-11 10:40:16 -0400

    Add Hello World code.

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World Fed. Today's date is "+ new Date());
+    }
+}
